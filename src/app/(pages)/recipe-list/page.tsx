022fcb7026a17484e1cbb3753d957b44281f4cdb
Avigail Shklovsky <9013825@gmail.com>
2024-11-12 15:42:48 +0200

    test 1

--- src/app/(pages)/recipe-list/page.tsx
+++ src/app/(pages)/recipe-list/page.tsx
@@ -1,21 +1,20 @@
-'use client'
-import React, { useEffect } from 'react';
-import { useRecipeStore } from '@/app/store/use-store/useRecipeStore';
+"use client";
+import React, { useEffect } from "react";
+import { useRecipeStore } from "@/app/store/use-store/useRecipeStore";
 
 const Page = () => {
+  console.log("in");
 
-    const fetchRecipes = useRecipeStore((state) => state.fetchRecipes);
-    const recipeList = useRecipeStore((state) => state.recipeList);
+  const fetchRecipes = useRecipeStore((state) => state.fetchRecipes);
+  const recipeList = useRecipeStore((state) => state.recipeList);
 
-    useEffect(() => {
-        fetchRecipes();
-    })
+  useEffect(() => {
+    fetchRecipes();
+  });
 
-    return (
-        <>
-            {recipeList && recipeList.map((r) => <h1 key={r._id}>{r.name}</h1>)}
-        </>
-    )
-}
+  return (
+    <>{recipeList && recipeList.map((r) => <h1 key={r._id}>{r.name}</h1>)}</>
+  );
+};
 
 export default Page;
