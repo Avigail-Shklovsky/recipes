1a952958dbbdb8866a3f5f19fdee0e24440d363b
Avigail Shklovsky <9013825@gmail.com>
2024-11-12 15:00:13 +0200

    card ugly

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: ["tinyurl.com"],
+  },
 };
 
 export default nextConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,8 @@
+import Card from "./components/Card";
+
 export default function Home() {
   return (
-    <></>
+    <>
+    <Card _id="1" ingredients={['ghjkl']} name={'recipe'} category={'category'} imageUrl={'https://tinyurl.com/bddvtauz'} instructions={'instructions sdfghjkl;;jklkjhgbn'} isFavorite={false}></Card></>
   );
 }
