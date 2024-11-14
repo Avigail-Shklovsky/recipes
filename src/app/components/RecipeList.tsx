a8f8794259da00b4166bb541287efbc782da4b3b
Avigail Shklovsky <9013825@gmail.com>
2024-11-14 09:18:05 +0200

    r

--- src/app/components/RecipeList.tsx
+++ src/app/components/RecipeList.tsx
@@ -10,56 +10,59 @@ const RecipeList = () => {
   const [isOpen, setIsOpen] = useState(false);
   const fetchRecipes = useRecipeStore((state) => state.fetchRecipes);
   const recipeList = useRecipeStore((state) => state.recipeList);
-  const setCurrentRecipe=useRecipeStore((state)=>state.setCurrentRecipe);
+  const setCurrentRecipe = useRecipeStore((state) => state.setCurrentRecipe);
 
   useEffect(() => {
     fetchRecipes();
   }, []);
 
-  const openOrCloseModel=(recipe:Recipe)=>{
-    setCurrentRecipe(recipe)
-    setIsOpen(prev=>!prev);
-  }
+  const openOrCloseModel = (recipe: Recipe) => {
+    setCurrentRecipe(recipe);
+    setIsOpen((prev) => !prev);
+  };
 
-  const closeModal=()=>{
+  const closeModal = () => {
     setIsOpen(false);
-    setCurrentRecipe(null)
-  }
+    setCurrentRecipe(null);
+  };
   return (
     <div>
       <div className="mt-5 ml-5">
-      {recipeList && (
-        <div className=" grid grid-cols-6 gap-2">
-          {recipeList.map((r) => (
-            <Card key={r._id} recipe={r} openCard={()=>openOrCloseModel(r)} ></Card>
-          ))}
-        </div>
-      )}
+        {recipeList && (
+          <div className=" grid grid-cols-6 gap-20">
+            {recipeList.map((r) => (
+              <Card
+                key={r._id}
+                recipe={r}
+                openCard={() => openOrCloseModel(r)}
+              ></Card>
+            ))}
+          </div>
+        )}
       </div>
       {isOpen && (
-        <div 
+        <div
           className="fixed inset-0 bg-black bg-opacity-50 z-40"
           onClick={closeModal}
         />
       )}
 
-      <div 
+      <div
         className={`fixed top-0 right-0 h-full w-80 bg-white shadow-lg z-50 p-4 transition-transform duration-300 ${
-          isOpen ? 'translate-x-0' : 'translate-x-full'
+          isOpen ? "translate-x-0" : "translate-x-full"
         }`}
       >
-        <button 
+        <button
           onClick={closeModal}
           className="mb-4 text-gray-500 hover:text-gray-800"
         >
           ✖
         </button>
 
-        <RecipeModel/>
+        <RecipeModel />
       </div>
     </div>
   );
 };
 
 export default RecipeList;
-
