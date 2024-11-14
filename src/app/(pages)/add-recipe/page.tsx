a1f43f88ab8549ceacbd2c4159e6645ce21bde94
Avigail-Shklovsky <148010735+Avigail-Shklovsky@users.noreply.github.com>
2024-11-14 16:11:17 +0200

    Update page.tsx

--- src/app/(pages)/add-recipe/page.tsx
+++ src/app/(pages)/add-recipe/page.tsx
@@ -1,10 +1,8 @@
 'use client'
 import React from 'react'
-import { useRouter } from 'next/navigation';
 import AddRecipe from '@/app/components/AddRecipe';
 
 const Page = () => {
-    const router = useRouter();
 
     return (
         <div>
