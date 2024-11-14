93f013cf0d1565669dc5f913b75b416081d6a021
Avigail-Shklovsky <148010735+Avigail-Shklovsky@users.noreply.github.com>
2024-11-14 16:10:38 +0200

    Update page.tsx

--- src/app/(pages)/add-recipe/page.tsx
+++ src/app/(pages)/add-recipe/page.tsx
@@ -3,15 +3,14 @@ import React from 'react'
 import { useRouter } from 'next/navigation';
 import AddRecipe from '@/app/components/AddRecipe';
 
-const page = () => {
+const Page = () => {
     const router = useRouter();
 
     return (
         <div>
-            <button onClick={() => { router.push('/') }} className='text-[#404445] font-normal mt-[30px] ml-[30px]'>‹ Back</button>
             <AddRecipe />
         </div>
     )
 }
 
-export default page;
+export default Page;
