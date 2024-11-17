a16285115877accbeb15b6a4f1dfcd4aaee92c3b
rachel rapaport <rachelport283@gmail.com>
2024-11-17 12:06:44 +0200

    testCache 5

--- next.config.js
+++ next.config.js
@@ -7,20 +7,7 @@ const nextConfig = {
       "adikosh.co.il",
       "encrypted-tbn0.gstatic.com",
     ],
-    headers: [
-      {
-        key: "Cache-Control",
-        value: "no-store, no-cache, must-revalidate, proxy-revalidate",
-      },
-      {
-        key: "Pragma",
-        value: "no-cache",
-      },
-      {
-        key: "Expires",
-        value: "0",
-      },
-    ],
+  
   },
 };
 
