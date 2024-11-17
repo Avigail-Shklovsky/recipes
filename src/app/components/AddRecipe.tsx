6da6d843226a12702cce0cb3545d5e1d19b234a0
Shira Biton <shirabiton387@gmail.com>
2024-11-17 14:12:49 +0200

    Fix bugs in AddRecipe component because of ingredients error last change

--- src/app/components/AddRecipe.tsx
+++ src/app/components/AddRecipe.tsx
@@ -20,30 +20,14 @@ const formSchema = z.object({
     .url("Invalid URL")
     .trim()
     .min(1, "Image URL is required"),
-  ingredients: z
-    .array(
-      z
-        .string()
-        .trim()
-        .min(3, "Recipe name must contain at least 3 characters")
-        .max(60, "Recipe name must contain at most 60 characters")
-        .min(1, "Recipe name is required"),
-    category: z.string().trim().min(1, "Category is required"),
-    imageUrl: z
-        .string()
-        .url("Invalid URL")
-        .trim()
-        .min(1, "Image URL is required"),
-    ingredients: z
-        .array(z.string())
-        .min(1, "Ingredient is required"),
-    instructions: z
-        .string()
-        .trim()
-        .min(20, "Instructions must contain at least 20 characters")
-        .max(600, "Ingredient must contain at most 600 characters")
-        .min(1, "Instructions are required"),
-    isFavorite: z.boolean(),
+  ingredients: z.array(z.string()).min(1, "Recipe name is required"),
+  instructions: z
+    .string()
+    .trim()
+    .min(20, "Instructions must contain at least 20 characters")
+    .max(600, "Ingredient must contain at most 600 characters")
+    .min(1, "Instructions are required"),
+  isFavorite: z.boolean(),
 });
 
 const AddRecipe = () => {
@@ -61,7 +45,7 @@ const AddRecipe = () => {
   const [errors, setErrors] = useState<{ [key: string]: string }>({});
 
   const categoryList = useRecipeStore(
-    (state: { categoryList:string[] }) => state.categoryList
+    (state: { categoryList: string[] }) => state.categoryList
   );
   const [ingredient, setIngredient] = useState<string>("");
 
@@ -112,80 +96,56 @@ const AddRecipe = () => {
     }
   };
 
-    return (
-        <>
-            <button
-                onClick={() => {
-                    router.push("/");
-                }}
-                className="text-[#404445] font-normal mt-[30px] ml-[30px]"
-            >
-                ‹ Back
-            </button>
-            <div className='p-[50px]'>
-                <h1 className='text-[4vh] pb-[6vh]'>Add Recipe</h1>
-                <form onSubmit={handleSubmit} className='flex gap-[5vw]'>
-                    <div className='flex flex-col gap-[5vh]'>
-                        <span>
-                            <input type="text" placeholder='Recipe name' name='name' value={formData.name} className='input-box' onChange={handleChange} />
-                            {errors.name && <p className='error-message'>{errors.name}</p>}
-                        </span>
-                        <span>
-                            <select name="category" id="category" defaultValue="" onChange={handleChange} className='w-[30vw] md:w-[15vw] rounded-md px-[10px] py-[3px] border-[0.5px] border-gray-300 border-solid text-[#A2A4B5] cursor-pointer focus:text-black'>
-                                <option value="" disabled>Category</option>
-                                {categoryList && categoryList.map((c: string) => <option value={c} key={c}>{c}</option>)}
-                            </select>
-                            {errors.category && <p className='error-message'>{errors.category}</p>}
-                        </span>
-                        <span>
-                            <input type="url" placeholder='Image URL' name='imageUrl' value={formData.imageUrl} className='input-box' onChange={handleChange} />
-                            {errors.imageUrl && <p className='error-message'>{errors.imageUrl}</p>}
-                        </span>
-                        <div className='flex flex-row gap-[1vw]'>
-                            <span>
-                                <input type="text" placeholder='Ingredients' name='ingredient' value={ingredient} className='input-box' onChange={(e) => setIngredient(e.target.value)} />
-                                <ul>{formData.ingredients.map((ingredient) => (<li key={ingredient}>{ingredient}</li>))}</ul>
-                                {errors.ingredients && <p className='error-message'>{errors.ingredients}</p>}
-                            </span>
-                            <input type="button" value="+" onClick={addIngredientHandler} className='flex justify-center content-center bg-[#7864EA] text-white rounded-sm px-[6px] w-[30px] h-[30px] cursor-pointer' />
-                        </div>
-                    </div>
-                    <div className='flex flex-col gap-[4vh]'>
-                        <span>
-                            <textarea
-                                placeholder='Instructions'
-                                name='instructions'
-                                value={formData.instructions}
-                                className='input-box w-[27vw] h-[29vh] md:w-[50vw] md:h-[32vh]'
-                                onChange={handleChange}
-                            />
-                            {errors.instructions && <p className='error-message min-w-[40vw]'>{errors.instructions}</p>}
-                        </span>
-                        <input type="submit" value="Add" className='flex self-end bg-[#7864EA] text-white rounded-md p-[7px] w-[7vw] cursor-pointer' />
-                    </div>
-                </form>
+  return (
+    <>
+      <button
+        onClick={() => {
+          router.push("/");
+        }}
+        className="text-[#404445] font-normal mt-[30px] ml-[30px]"
+      >
+        ‹ Back
+      </button>
+      <div className='p-[50px]'>
+        <h1 className='text-[4vh] pb-[6vh]'>Add Recipe</h1>
+        <form onSubmit={handleSubmit} className='flex gap-[5vw]'>
+          <div className='flex flex-col gap-[5vh]'>
+            <span>
+              <input type="text" placeholder='Recipe name' name='name' value={formData.name} className='input-box' onChange={handleChange} />
+              {errors.name && <p className='error-message'>{errors.name}</p>}
+            </span>
+            <span>
+              <select name="category" id="category" defaultValue="" onChange={handleChange} className='w-[30vw] md:w-[15vw] rounded-md px-[10px] py-[3px] border-[0.5px] border-gray-300 border-solid text-[#A2A4B5] cursor-pointer focus:text-black'>
+                <option value="" disabled>Category</option>
+                {categoryList && categoryList.map((c: string) => <option value={c} key={c}>{c}</option>)}
+              </select>
+              {errors.category && <p className='error-message'>{errors.category}</p>}
+            </span>
+            <span>
+              <input type="url" placeholder='Image URL' name='imageUrl' value={formData.imageUrl} className='input-box' onChange={handleChange} />
+              {errors.imageUrl && <p className='error-message'>{errors.imageUrl}</p>}
+            </span>
+            <div className='flex flex-row gap-[1vw]'>
+              <span>
+                <input type="text" placeholder='Ingredients' name='ingredient' value={ingredient} className='input-box' onChange={(e) => setIngredient(e.target.value)} />
+                <ul>{formData.ingredients.map((ingredient) => (<li key={ingredient}>{ingredient}</li>))}</ul>
+                {errors.ingredients && <p className='error-message'>{errors.ingredients}</p>}
+              </span>
+              <input type="button" value="+" onClick={addIngredientHandler} className='flex justify-center content-center bg-[#7864EA] text-white rounded-sm px-[6px] w-[30px] h-[30px] cursor-pointer' />
             </div>
           </div>
-          <div className="flex flex-col gap-[4vh]">
+          <div className='flex flex-col gap-[4vh]'>
             <span>
               <textarea
-                placeholder="Instructions"
-                name="instructions"
+                placeholder='Instructions'
+                name='instructions'
                 value={formData.instructions}
-                className="input-box instructions-input"
+                className='input-box w-[27vw] h-[29vh] md:w-[50vw] md:h-[32vh]'
                 onChange={handleChange}
               />
-              {errors.instructions && (
-                <p className="error-message min-w-[40vw]">
-                  {errors.instructions}
-                </p>
-              )}
+              {errors.instructions && <p className='error-message min-w-[40vw]'>{errors.instructions}</p>}
             </span>
-            <input
-              type="submit"
-              value="Add"
-              className="flex self-end bg-[#7864EA] text-white rounded-md p-[7px] w-[7vw] cursor-pointer"
-            />
+            <input type="submit" value="Add" className='flex self-end bg-[#7864EA] text-white rounded-md p-[7px] w-[7vw] cursor-pointer' />
           </div>
         </form>
       </div>
