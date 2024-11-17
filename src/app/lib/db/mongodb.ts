42c6b7ccd34104bdae7b6ce071996613be9544d1
rachel-rapaport <rachelport283@gmail.com>
2024-11-17 14:22:05 +0200

    connect

--- src/app/lib/db/mongodb.ts
+++ src/app/lib/db/mongodb.ts
@@ -2,10 +2,16 @@ import mongoose from "mongoose";
 const MONGODB_URI = process.env.MONGODB_URI || "";
 
 // connecting to database- mongoDB.
+let isConnected = false;
 
 const connect = async () => {
+  if (isConnected) {
+    console.log("Already connected to MongoDB");
+    return;
+  }
   try {
-    await mongoose.connect(MONGODB_URI);
+    const db = await mongoose.connect(MONGODB_URI);
+    isConnected = db.connection.readyState === 1;
     console.log("mongodb connection seccseefull");
   } catch (error) {
     throw new Error("error in connection to mongodb " + error);
